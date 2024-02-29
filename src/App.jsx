7e4e07d03e007a1dc0824605e3dc47128787d245
piyushmishraa <fanboiofharrystyles@gmail.com>
2024-02-29 08:22:00 +0530

    setup Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Program from './Program';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const appRouter= createBrowserRouter([
+    {
+      path:"/",
+      element:<Login/>
+
+    },
+   
+  ])
 
   return (
-    <>
-    <div className='text-3xl font-bold underline'>hello</div>
-    </>
+    
+     <div>
+     <RouterProvider router={appRouter}/>
+    </div>
+    
   )
 }
 
---
+++ src/Program.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Program = () => {
+  return (
+    <div className='bg-red'>Program</div>
+  )
+}
+
+export default Program
